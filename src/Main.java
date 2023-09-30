d78c4e30a11572de58d60bdbe456c8b5b14f6589
BirdMelo <jpmn2013.terra.com.br@gmail.com>
2023-09-30 13:01:11 -0300

    corrigido.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 import Operations.Address;
 import Operations.SearchHttp;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+        Scanner write = new Scanner(System.in);
+        System.out.print("CEP: ");
+        var cep = write.nextLine();
         SearchHttp search = new SearchHttp();
-        Address newAddress = search.http("71645110");
+        Address newAddress = search.http(cep);
         System.out.println(newAddress);
     }
 }
